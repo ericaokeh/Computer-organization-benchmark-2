05b084ca8da290d30b9a4534b7ab23010a894ce7
ericaokeh <157079075+ericaokeh@users.noreply.github.com>
2025-03-26 22:55:20 -0400

    Add hard drive chunk size constants for small and large operations

--- benchmarks.asm
+++ benchmarks.asm
@@ -4,4 +4,6 @@ section .data
     INT_MUL_COUNT    equ 5000000000    ; 5 × 10^9
     INT_DIV_COUNT    equ 2000000000    ; 2 × 10^9
     MEM_OP_COUNT     equ 5000000000    ; 5 × 10^9
-    HD_READ_COUNT    equ 1000000000    ; 10^9 
+    HD_READ_COUNT    equ 1000000000    ; 10^9
+    HD_CHUNK_SMALL   equ 100
+    HD_CHUNK_LARGE   equ 10000 
