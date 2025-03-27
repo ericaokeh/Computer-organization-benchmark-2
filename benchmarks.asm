07bfff239f994295e0a043b3e41e261f03d530b9
ericaokeh <157079075+ericaokeh@users.noreply.github.com>
2025-03-26 22:55:10 -0400

    Add memory and hard drive operation constants

--- benchmarks.asm
+++ benchmarks.asm
@@ -2,4 +2,6 @@ section .data
     ; Constants for benchmarks
     INT_ADD_COUNT    equ 1010
     INT_MUL_COUNT    equ 5000000000    ; 5 × 10^9
-    INT_DIV_COUNT    equ 2000000000    ; 2 × 10^9 
+    INT_DIV_COUNT    equ 2000000000    ; 2 × 10^9
+    MEM_OP_COUNT     equ 5000000000    ; 5 × 10^9
+    HD_READ_COUNT    equ 1000000000    ; 10^9 
